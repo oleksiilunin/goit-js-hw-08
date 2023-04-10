590c2f387eb001f4a4468440ba174340cde63a91
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-04-10 11:31:31 +0300

    copy js-code from hw-07

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,33 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
-console.log("Hello");
+const markupGallery = document.querySelector('.gallery');
+
+(function () {
+	const markup = galleryItems.map(({ preview, original, description }) => `
+	<li class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img
+				class="gallery__image"
+				src="${preview}"
+				data-source="${original}"
+				alt="${description}"
+			/>
+		</a>
+	</li>
+	`).join('');
+	markupGallery.insertAdjacentHTML('beforeend', markup)
+})();
+
+const optionsForCaption = {
+	captionsData: 'alt',
+	captionDelay: 250,
+};
+
+const gallery = new SimpleLightbox('.gallery__link', optionsForCaption);
