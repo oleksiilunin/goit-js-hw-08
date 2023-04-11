19d72ce20465265c23ecc45165520914eb1a93ff
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-04-11 14:40:51 +0300

    Create js-code in 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+// import trottle from 'lodash.trottle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(saveCurrentTime,1000));
+
+function saveCurrentTime (data) {
+	currentTime = data.seconds;
+	localStorage.setItem("videoplayer-current-time", currentTime);
+};
+
+player.setCurrentTime(Number(localStorage.getItem(STORAGE_KEY)));
