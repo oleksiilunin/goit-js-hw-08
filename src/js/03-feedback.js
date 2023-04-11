63b07ca473eb4985be81e3a476c3a4f7d1f5d9f8
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-04-11 19:05:51 +0300

    Create js-code in 03-feedback.js fo goit-js-hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const STORAGE_KEY = "feedback-form-state";
+let dataFeedback = {};
+console.log(feedbackForm);
 
+
+feedbackForm.addEventListener('input', onInputText);
+feedbackForm.addEventListener('submit', onSubmit);
+
+fillTextarea();
+
+function onInputText(evt) {
+	const { email, message } = evt.currentTarget.elements;
+
+	dataFeedback = { email: email.value, message: message.value };
+
+	localStorage.setItem(STORAGE_KEY, JSON.stringify(dataFeedback));
+}
+
+function onSubmit(evt) {
+	evt.preventDefault();
+	console.log(dataFeedback);
+	evt.currentTarget.reset();
+}
+
+function fillTextarea() {
+	const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+	if (savedData) {
+		const { email, message } = savedData;
+
+		const inputEmail = document.querySelector('[name ="email"]');
+		const inputMessage = document.querySelector('[name ="message"]');
+
+		inputEmail.value = email;
+		inputMessage.value = message;
+	}
+
+}
